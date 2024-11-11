4ebed7859ea446a234d3475466a5caf439cb2722
kubik08 <jakub.kreisinger.024@pslib.cz>
2024-11-11 09:59:24 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,17 @@
 
-let buttonWasPressed: boolean;
+let buttonWasPressed: number = 0; // 0 bude nestisknuto a 1 bude stisknuto
+
+while (true){
+
+    if (input.buttonIsPressed(Button.A)) {
+     let buttonWasPressed = 1;
+     
+ }
+else{
+    buttonWasPressed = 0
+}
+buttonWasPressed = input.buttonIsPressed(Button.A) ? 1 : 0
+console.logValue("tlacitko", buttonWasPressed)
+ basic.pause(50); // 50ms = 20x za sekundu
+}
+basic.showIcon(IconNames.Heart)
